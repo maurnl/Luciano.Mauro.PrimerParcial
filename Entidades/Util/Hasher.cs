39217bb375367115c590a0a0288b945d913f078c
maurnl <mauronluciano@hotmail.com>
2022-09-20 20:04:11 -0300

    renamed variable on Hasher class

--- Entidades/Util/Hasher.cs
+++ Entidades/Util/Hasher.cs
@@ -14,8 +14,8 @@ namespace Parcial.Util
         /// <returns></returns>
         public static string HashText(string text, HashAlgorithm hasher)
         {
-            byte[] textWithSaltBytes = Encoding.UTF8.GetBytes(text);
-            byte[] hashedBytes = hasher.ComputeHash(textWithSaltBytes);
+            byte[] textBytes = Encoding.UTF8.GetBytes(text);
+            byte[] hashedBytes = hasher.ComputeHash(textBytes);
             hasher.Clear();
             return Convert.ToBase64String(hashedBytes);
         }
