e819e2e9fdf160e07dce50dc9c6c7abd5e5d7469
maurnl <mauronluciano@hotmail.com>
2022-09-21 17:13:12 -0300

    sobrecarga de operadores en Puerto

--- Entidades/Entities/Puerto.cs
+++ Entidades/Entities/Puerto.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace Parcial.Entities
 {
-    class Puerto
+    public class Puerto
     {
+        private Pais pais;
+        private Provincia provincia;
+
+        public Pais Pais
+        {
+            get
+            {
+                return this.pais;
+            }
+        }
+        public Provincia Provincia
+        {
+            get
+            {
+                return this.provincia;
+            }
+        }
+
+        public Puerto(Provincia provincia, Pais pais)
+        {
+            this.provincia = provincia;
+            this.pais = pais;
+        }
+
+        public static bool operator ==(Puerto puertoA, Puerto puertoB)
+        {
+            bool returnAux;
+            if (puertoA is null)
+            {
+                returnAux = puertoB is null;
+            }
+            else if (puertoB is null)
+            {
+                returnAux = puertoA is null;
+            }
+            else
+            {
+                returnAux = puertoA.Pais == puertoB.Pais && puertoA.Provincia == puertoB.Provincia;
+            }
+            return returnAux;
+        }
+        public static bool operator !=(Puerto puertoA, Puerto puertoB)
+        {
+            return !(puertoA == puertoB);
+        }
+        public override string ToString()
+        {
+            return $"{this.provincia}, {this.pais}";
+        }
     }
 }
